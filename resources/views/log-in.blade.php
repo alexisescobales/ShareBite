bbd2f216b00ca3c5eb417967f94a552f9e4584fd
alexisescobales <99324319+alexisescobales@users.noreply.github.com>
2024-03-05 16:00:41 +0100

    log-in

--- resources/views/log-in.blade.php
+++ resources/views/log-in.blade.php
@@ -1,11 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
+    <link rel="stylesheet" href="../resources/css/log-in.css">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>SHAREBITES</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    
+    <div class="header">
+        <h1 class="title">SHARE<span class="amarillo">BITES</span></h1>
+        <img src="../resources/img/icon_white.svg" alt="Logo" class="logo">
+    </div>
+
+
+    <div class="login_div"></div>
+
+
+    <div class="v-line"></div>
+
 </body>
 </html>
