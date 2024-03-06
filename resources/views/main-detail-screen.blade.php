214560ae02b0ae002c88d61ca4e4e3ad5fe51d3a
Mario23leiva <mleiva@alumno.santoangel.org>
2024-03-06 20:22:23 +0100

    sadf

---
+++ resources/views/main-detail-screen.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/app.scss'])
+    <script src="https://kit.fontawesome.com/6645ba3d64.js" crossorigin="anonymous"></script>
+    <title>aaaaa</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-md">
+        <div class="container-fluid justify-content-between">
+            <a class="navbar-brand" href="#">
+                <h1 class="title">SHARE<span class="amarillo">BITES</span></h1>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <i class="fa-solid fa-house activo"></i>
+                    </li>
+                    <li class="nav-item">
+                        <i class="fa-solid fa-chart-simple"></i>
+                    </li>
+                    <li class="nav-item">
+                        <i class="fa-solid fa-user"></i>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="row main-container">
+        <div class="col-4 left-column">
+            <h3>¿QUE BUSCAS?</h3>
+            <button>Puntos de entrega</button>
+            <button>Puntos de recogida</button>
+        </div>
+        <div class="col">
+
+        </div>
+    </div>
+</body>
+
+</html>
