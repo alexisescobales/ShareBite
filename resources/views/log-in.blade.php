fb4a641bed19be11101e5b4d993808bfb594ce90
alexisescobales <99324319+alexisescobales@users.noreply.github.com>
2024-03-07 19:19:13 +0100

    log-in, botones, header, inputs

--- resources/views/log-in.blade.php
+++ resources/views/log-in.blade.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
-    <link rel="stylesheet" href="../resources/css/log-in.css">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SHAREBITES</title>
+    <link rel="stylesheet" href="../resources/css/log-in.css">
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
@@ -13,16 +13,28 @@
         <img src="../resources/img/icon_white.svg" alt="Logo" class="logo">
     </div>
 
-
     <div class="login_div">
-        <input type="text" placeholder="Nombre de usuario">
-        <input type="password" placeholder="Contraseña">
-        <button class="login">Ingresar</button>
+        <div class="inputs">
+            <div class="input-wrapper">
+                <div class="input-label">
+                    <img class="icons_login" src="../resources/img/user.png" alt="user_icon">
+                    <h2 class="subtitle">CORREO</h2>
+                </div>
+                <input type="text">
+            </div>
+            <div class="input-wrapper">
+                <div class="input-label">
+                    <img class="icons_login" src="../resources/img/password.png" alt="lock_icon">
+                    <h2 class="subtitle">CONTRASEÑA</h2>
+                </div>
+                <input type="password">
+            </div>
+        </div>
+
+        <button class="btn" id="login">LOGIN</button>
+        <button class="btn" id="registro">REGISTER</button>
     </div>
 
-
-
     <div class="v-line"></div>
-
 </body>
 </html>
