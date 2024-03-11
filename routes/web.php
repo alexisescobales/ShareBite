fc9db53d5e8c9aec6a35c61b866ccfe8c5dd9c0e
Mario23leiva <mleiva@alumno.santoangel.org>
2024-03-11 20:01:30 +0100

    asd

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('main-detail-screen');
+    return view('principal');
 });
 
 Route::get('/main-screen', function () {
