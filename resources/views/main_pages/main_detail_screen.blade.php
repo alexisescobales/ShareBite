6cd0341afe09a2b34cc0e6fe3a82325c338b99e9
Mario23leiva <mleiva@alumno.santoangel.org>
2024-03-12 17:01:31 +0100

    arreglo bootstrap

--- resources/views/main_pages/main_detail_screen.blade.php
+++ resources/views/main_pages/main_detail_screen.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/app.scss'])
+    @vite(['resources/js/app.js', 'resources/css/app.scss'])
     <script src="https://kit.fontawesome.com/6645ba3d64.js" crossorigin="anonymous"></script>
     <title>aaaaa</title>
     <script src='https://api.mapbox.com/mapbox-gl-js/v2.7.0/mapbox-gl.js'></script>
