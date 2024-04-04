401c5ff4ef7c769efd34ef06f4c5cfd4f89a53a3
rachidjkl <rghenema2122@politecnics.barcelona>
2024-04-04 19:39:52 +0200

    xfg

--- app/Models/tipo_marca.php
+++ app/Models/tipo_marca.php
@@ -2,21 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Tipo_marca extends Model
+class Tipo_Marca extends Model
 {
-    use HasFactory;
+    protected $table = 'tipo_marca'; // Nombre de la tabla en la base de datos
+    protected $fillable = ['id_tipo_marca', 'nombre_marca'];
 
-    protected $table = 'tipo_marca';
-    protected $primaryKey = 'id_tipo';
-    public $incrementing = false;
-    public $timestamps = false;
-
-    public function marcas(): HasMany
+    // Relación con la tabla marcas
+    public function marcas()
     {
-        return $this->hasMany(Marcas::class);
+        return $this->hasMany(Marca::class, 'tipo_marca_id_tipo_marca', 'id');
     }
 }
