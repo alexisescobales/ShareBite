26f7deff5ca8ad7ef5e52e25f933721733bf53b9
alexisescobales <99324319+alexisescobales@users.noreply.github.com>
2024-04-10 19:48:45 +0200

    puas con icono y popups en mapbox

--- resources/views/main_pages/lista_proveedores.blade.php
+++ resources/views/main_pages/lista_proveedores.blade.php
@@ -16,27 +16,21 @@
         </div>
         <ul class="main_list">
             @foreach ($tiendas as $tienda)
-            <li>
-                <div>
-                    <img src="../resources/img/tienda.png" alt="">
-                    <div>
-                        <h4>{{ $tienda->nombre }}</h4>
-                        <ul>
-                            @foreach ($tienda->tiendas as $tienda_individual)
-                                <p>Dirección: {{ $tienda_individual->direccion }}</p>
-                                <p>Lotes: {{ $tienda_individual->menus }}</p>
-                                <ul>
-                                    @foreach ($tienda_individual->marca as $marca_individual)
-                                    @endforeach
-                                </ul>
-                            @endforeach
-                        </ul>
+                <li>
+                    <div class="tienda" data-lat="{{ $tienda->tiendas[0]->marca->lat }}" data-long="{{ $tienda->tiendas[0]->marca->long }}" data-nombre="{{ $tienda->nombre }}" data-direccion="{{ $tienda->tiendas[0]->direccion }}" data-lotes="{{ $tienda->tiendas[0]->menus }}">
+                        <img src="../resources/img/tienda.png" alt="">
+                        <div>
+                            <h4>{{ $tienda->nombre }}</h4>
+                            <ul>
+                                @foreach ($tienda->tiendas as $tienda_individual)
+                                    <p>Dirección: {{ $tienda_individual->direccion }}</p>
+                                    <p>Lotes: {{ $tienda_individual->menus }}</p>
+                                @endforeach
+                            </ul>
+                        </div>
                     </div>
-                    <!-- Aquí puedes calcular la distancia desde la ubicación actual si es necesario -->
-                </div>
-            </li>
-        @endforeach
-        
+                </li>
+            @endforeach
         </ul>
     </div>
 @endsection
@@ -51,32 +45,53 @@
     <script>
         // Convertimos la variable PHP a JSON
         let tiendas = @json($tiendas);
-    
+
         // Función para agregar marcadores
         function addMarkers(tiendas) {
             tiendas.forEach(function(tienda) {
                 tienda.tiendas.forEach(function(tienda_individual) {
-                    // Accedemos a la propiedad "marca" de cada tienda_individual
                     let marca = tienda_individual.marca;
-    
-                    // Verificamos si la marca es un objeto válido
                     if (marca) {
-                        // Obtenemos la latitud y longitud de la marca
                         let lat = marca.lat;
                         let long = marca.long;
-    
-                        // Verificamos que tengamos valores válidos de latitud y longitud
                         if (lat && long) {
-                            // Agregamos un marcador al mapa con la latitud y longitud
-                            new mapboxgl.Marker()
-                                .setLngLat([parseFloat(long), parseFloat(lat)]) // Convertimos a números flotantes
+                            // Crear un elemento de imagen para el marcador
+                            let el = document.createElement('img');
+                            el.src = "../resources/img/tienda_pua.png"; // Ruta de la imagen del marcador
+                            el.style.width = '40px'; // Ajusta el ancho según tus preferencias
+                            el.style.height = '40px'; // Ajusta la altura según tus preferencias
+
+                            // Crear el marcador con el elemento de imagen personalizado
+                            let marker = new mapboxgl.Marker(el)
+                                .setLngLat([parseFloat(long), parseFloat(lat)])
                                 .addTo(map);
+
+                            // Agregar popup al marcador
+                            let popup = new mapboxgl.Popup()
+                                .setHTML(`<h4>${tienda.nombre}</h4><p>Dirección: ${tienda_individual.direccion}</p><p>Lotes: ${tienda_individual.menus}</p>`);
+
+                            marker.setPopup(popup);
                         }
                     }
                 });
             });
+
+            // Agregar evento de clic a todos los divs de la clase 'tienda'
+            document.querySelectorAll('.tienda').forEach(function(tiendaDiv) {
+                tiendaDiv.addEventListener('click', function() {
+                    let lat = tiendaDiv.getAttribute('data-lat');
+                    let long = tiendaDiv.getAttribute('data-long');
+
+                    // Hacer zoom en la ubicación de la tienda
+                    map.flyTo({
+                        center: [long, lat],
+                        zoom: 15, // Nivel de zoom deseado
+                        essential: true // Marcar como esencial para evitar el bloqueo por el navegador
+                    });
+                });
+            });
         }
-    
+
         // Llamamos a la función para agregar los marcadores
         addMarkers(tiendas);
     </script>
