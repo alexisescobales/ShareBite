f9f454c66ccb55f3edd657058dbb9e9a27d9e5f2
rachidjkl <rghenema2122@politecnics.barcelona>
2024-04-18 17:27:19 +0200

    vv

--- resources/views/login_pages/selection.blade.php
+++ resources/views/login_pages/selection.blade.php
@@ -11,7 +11,7 @@
     <form style="margin: 10px" action="{{ action([App\Http\Controllers\ControlerUsuario::class, 'seleccion'], ['eleccion' => 'proveedor']) }}" method="POST">
         @csrf
         <div class="selection_group">
-            <button class="btn_grande" type="submit">PROVEEDOR <i class="fa-solid fa-bicycle"></i></button>
+            <button class="btn_grande" type="submit">PROVEEDOR <i class="fa-solid fa-shop"></i></button>
         </div>
     </form>
 </div>
